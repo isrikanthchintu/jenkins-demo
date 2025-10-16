3e47f3dda98dea34f9bc1ff76ec6fb0399e91173
Srikanth <shiva.kotha@cognixsoft.com>
2025-10-16 17:47:51 +0530

    added student class

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
-        System.out.println("Welcome to Jenkins world!!!!!!!");
+        System.out.println("Welcome to Jenkins world!!!!!!!!!!");
 
     }
 }
