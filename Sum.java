fa6aa18e2df6bb491ee50d7d0aafe61462b85712
Srikanth <shiva.kotha@cognixsoft.com>
2025-10-16 18:05:19 +0530

    added student class

--- Sum.java
+++ Sum.java
@@ -1,8 +1,8 @@
-public class Sum {
-    public static void main(String[] args) {
-        int a = 5;
-        int b = 7;
-        int sum = a + b;
-        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
-    }
-}
+//public class Sum {
+//    public static void main(String[] args) {
+//        int a = 5;
+//        int b = 7;
+//        int sum = a + b;
+//        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+//    }
+//}
